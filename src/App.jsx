72d453eb6e1588919793d6b202d1e3e9239b949c
jhonviafara <jhon1144105@gmail.com>
2024-06-28 20:39:16 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
 import HistorialHerramientas from "./pages/historial-herramientas";
-function App() {
-  return (
-<<<<<<< HEAD
+function App() {  return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login/>} />
           <Route path="/historial-herramientas" element={<HistorialHerramientas/>} />
-        </Routes>
-      </BrowserRouter>
-=======
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login/>} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> 96a49ffd5bcfe0aed3e5a94c18cad7dcf3030bb8
+        </Routes>      </BrowserRouter>
   );
 }
 
--- src/pages/historial-herramientas.jsx
+++ src/pages/historial-herramientas.jsx
@@ -3,30 +3,31 @@ import StyledInput from "../components/StyledInput";
     
     function HistorialHerramientas() {
         return (
-            <div className="min-h-screen flex flex-col items-center">
+            <div className="bg-white min-h-screen flex flex-col items-center ">
              {/* Botones para agregar filtros */}
-            <div className="filtros-container">
-                <StyledInput textColor={"text-white"} type={"text"} placeholder={"nombre de usuario"} TLLabel={"filtrar por usuario"}/>
-                <StyledInput textColor={"text-white"} type={"text"} placeholder={"nombre de usuario"} TLLabel={"filtrar por herramienta"}/>
-                <StyledInput textColor={"text-white"} type={"text"} placeholder={"nombre de usuario"} TLLabel={"filtrar por fecha"}/>
+            <div className=" filtros-container gap-9 mb-3 flex w-25 ">
+                <StyledInput  textColor={"text-black"} type={"text"} placeholder={"filtrar por usuario"} />
+                <StyledInput textColor={"text-black"} type={"text"} placeholder={"filtrar por herramienta"} />
+                <StyledInput textColor={"text-black"} type={"text"} placeholder={"filtrar por fecha"} />
             </div>
             {/* Paginación  */}
-            <div role="tablist" className="tabs tabs-boxed ">
+            <div role="tablist" className="tabs tabs-boxed bg-gray-700 text-black">
               <a role="tab" className="tab">Tab 1</a>
               <a role="tab" className="tab tab-active">Tab 2</a>
               <a role="tab" className="tab">Tab 3</a>
             </div>
-      
             {/* Contenedor de la tabla */}
-            <div className="flex-grow justify-center w-full mb-10">
-              <table className="table table-zebra w-full">
+            <div className="flex-grow justify-center w-full mb-10 mt-4">
+              <table className="table text-black w-full ">
                 {/* cabecera */}
-                <thead>
-                  <tr>
+                <thead className="text-gray-700 bg-gray-300 ">
+                  <tr >
                     <th ></th>
                     <th >Usuario</th>
                     <th >Herramienta</th>
-                    <th >Fecha/Hora</th>
+                    <th>movimiento</th>
+                    <th >Fecha</th>
+                    <th>Hora</th>
                     <th >Estado de herramienta</th>
                   </tr>
                 </thead>
@@ -36,7 +37,9 @@ import StyledInput from "../components/StyledInput";
                     <th >1</th>
                     <td >Cy Ganderton</td>
                     <td >Quality Control Specialist</td>
-                    <td >3-03-2024/17:32</td>
+                    <td>prestada</td>
+                    <td >3-03-2024</td>
+                    <td>17:32</td>
                     <td >Rippio</td>
                   </tr>
                   {/* fila 2 */}
@@ -44,12 +47,23 @@ import StyledInput from "../components/StyledInput";
                     <th >2</th>
                     <td >Jorge</td>
                     <td >Martillo</td>
+                    <td>prestada</td>
                     <td >10-06-2024</td>
+                    <td>19:23</td>
                     <td >Vive</td>
+                  </tr>
+                  {/*fila 3*/}
+                  <tr>
+                    <th >1</th>
+                    <td >Cy Ganderton</td>
+                    <td >rusa</td>
+                    <td>devuelta</td>
+                    <td >3-03-2024</td>
+                    <td>17:32</td>
+                    <td >Rippio</td>
                   </tr>
                 </tbody>
               </table>
-      
               </div>
           </div>
         );
