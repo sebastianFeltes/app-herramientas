865f570edecbe26e87660a5f5a319cc8bff1a25d
puesto04cfl404 <cflpuesto04@gmail.com>
2024-07-08 18:30:13 -0300

    Primera subida

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+import AltaHerramientas from "./pages/alta-herramientas";
 
 function App() {
   return (
-    
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login/>} />
-        </Routes>
-      </BrowserRouter>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/alta-herramientas" element={<AltaHerramientas />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
