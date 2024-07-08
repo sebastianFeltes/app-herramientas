843c254b4e690e92d18af92e5d0700b7116b9ded
puesto02 <puesto02CFL404@gmail.com>
2024-07-08 17:42:57 -0300

    primera subida

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Inventario from "./pages/inventario";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/inventario" element={<Inventario />} />
       </Routes>
     </BrowserRouter>
   );
