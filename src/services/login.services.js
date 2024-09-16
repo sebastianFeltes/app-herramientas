1661c0d0d21fbf8bf4c4c9f2a03b04f1f3529268
RamiroJuncos <ramirog.juncos@gmail.com>
2024-09-16 17:50:58 -0300

    Cambios en login

--- src/services/login.services.js
+++ src/services/login.services.js
@@ -1,7 +1,21 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-export function postLogin(data){
-    let ruta = "/login";
-    let dataValidada = {};
-    let res = post(ruta , dataValidada)
+
+import { post, serverUrl } from "./utils.services";
+
+export async function postLogin(dni, pass) {
+  let ruta = "/login";
+  let dataValidada = {};
+  if (!dni || dni.length < 5) {
+    return "Dni Invalido";
+  } else if (!pass) {
+    return "Password Invalido";
+  } else {
+    dataValidada = { dni: parseInt(dni), password: pass };
+    // console.log(dataValidada);
+    let res = await post(ruta, dataValidada);
+    // console.log(res);
+    
+    return res
+  }
 }
