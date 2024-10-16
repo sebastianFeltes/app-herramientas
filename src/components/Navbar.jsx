afebfc652280db97d05a8811d15e72e1394ae7a5
RamiroJuncos <ramirog.juncos@gmail.com>
2024-10-16 20:14:19 -0300

    Miercoles16/10

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,32 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo-cfl.png";
 import StyledButton from "./StyledButton";
+import { useState } from "react";
+
+const links = [
+  {
+    text: "Inventario",
+    path: "/inventario",
+  },
+  {
+    text: "Historial Herramientas",
+    path: "/historial-herramientas",
+  },
+  {
+    text: "Alta Herramienta",
+    path: "/alta-herramientas",
+  },
+  {
+    text: "Lector QR",
+    path: "/lector-qr",
+  },
+];
+
 function Navbar() {
+  const nombreUs = localStorage.getItem("nombreUsuario").split("")[0];
+  const apellidoUs = localStorage.getItem("apellidoUsuario").split("")[0];
+  console.log(nombreUs, apellidoUs);
+
   return (
     <div>
       <div className="navbar bg-blue-700">
@@ -11,49 +36,70 @@ function Navbar() {
           </Link>
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
+          <ul className="menu menu-horizontal px-1 gap-2">
+            {links.map((link, index) => (
+              <li key={index}>
+                <Link
+                  className="btn min-w-56 bg-blue-700  border-gray-300 text-gray-200 hover:bg-blue-600 hover:text-white hover:underline hover:border-white hover:decoration-white "
+                  to={link.path}
+                >
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+            {/*  <li>
               <Link to={"/inventario"}>
-                <StyledButton
-                  link
-                  innerText={"Inventario"}
-                  btnType={"sumbit"}
-                />
+                <button className="btn min-w-56 bg-blue-700  border-gray-300 text-gray-200 hover:bg-blue-600 hover:text-white hover:underline hover:border-white hover:decoration-white">
+                  Inventario
+                </button>
               </Link>
             </li>
             <li>
               <Link to={"/historial-herramientas"}>
-                <StyledButton link innerText={"Historial"} btnType={"sumbit"} />
+                <button className="btn min-w-56 bg-blue-700  border-gray-300 text-gray-200 hover:bg-blue-600 hover:text-white hover:underline hover:border-white hover:decoration-white">
+                  Historial
+                </button>
               </Link>
             </li>
             <li>
               <Link to={"/alta-herramientas"}>
-                <StyledButton
-                  link
-                  innerText={"Alta Herramientas"}
-                  btnType={"sumbit"}
-                />
+                <button className="btn min-w-56 bg-blue-700  border-gray-300 text-gray-200 hover:bg-blue-600 hover:text-white hover:underline hover:border-white hover:decoration-white">
+                  Alta Herramienta
+                </button>
               </Link>
             </li>
             <li>
-              <Link to={"/lector-qr"}>
-                <StyledButton link innerText={"Lector QR"} btnType={"sumbit"} />
+              <Link
+                className="btn min-w-56 bg-blue-700  border-gray-300 text-gray-200 hover:bg-blue-600 hover:text-white hover:underline hover:border-white hover:decoration-white"
+                to={"/lector-qr"}
+              >
+                Lector Qr
               </Link>
-            </li>
-
-            {/* <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="bg-base-100 rounded-t-none p-2">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
             </li> */}
+            <div className="dropdown dropdown-end  flex">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar border border-white"
+              >
+                {nombreUs}
+                {apellidoUs}
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-blue-700 rounded-box z-[1] mt-3 w-52 p-2 shadow text-white"
+              >
+                <li>
+                  <a className="justify-between">Profile</a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
           </ul>
         </div>
       </div>
@@ -62,3 +108,9 @@ function Navbar() {
 }
 
 export default Navbar;
+
+/* 
+Tamaño mas chico de botones
+Hover dentro del boton
+Daisy Navbar Ul 
+*/
