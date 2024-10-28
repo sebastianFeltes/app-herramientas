c9301b249612dfc5b7647f6a55c397bd64610ae7
sebastianFeltes <jikansui16@gmail.com>
2024-10-28 19:37:40 -0300

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import logo from "../assets/logo-cfl.png";
 import StyledButton from "./StyledButton";
 import { useState } from "react";
 
-
 const links = [
   {
     text: "Inventario",
@@ -26,57 +25,57 @@ const links = [
 function Navbar() {
   const nombreUs = localStorage.getItem("nombreUsuario").split("")[0];
   const apellidoUs = localStorage.getItem("apellidoUsuario").split("")[0];
-  // console.log(nombreUs, apellidoUs); 
-  const location = useLocation()
+  // console.log(nombreUs, apellidoUs);
+  const location = useLocation();
 
   return (
-      <div className="navbar bg-blue-700 sticky top-0 z-50">
-        <div className="flex-1">
-          <Link to={"/home"} className="w-16">
-            <img className="w-full" src={logo} alt="" />
-          </Link>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1 gap-2">
-            {links.map((link, index) => (
-              <li key={index}>
-                <Link
-                  className="btn min-w-56 bg-blue-700  border-gray-300 text-gray-200 hover:bg-blue-600 hover:text-white hover:underline hover:border-white hover:decoration-white "
-                  to={link.path}
-                >
-                  {link.text}
-                </Link>
-              </li>
-            ))}
-            <div className="dropdown dropdown-end  flex">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar border border-white"
-              >
-                {nombreUs}
-                {apellidoUs}
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-blue-700 rounded-box z-[1] mt-3 w-52 p-2 shadow text-white"
-              >
-                <li>
-                  <a className="justify-between">Profile</a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-              </ul>
-            </div>
-          </ul>
-        </div>
+    <div className="navbar bg-blue-700 sticky top-0 z-50">
+      <div className="flex-1">
+        <Link to={"/home"} className="w-16">
+          <img className="w-full" src={logo} alt="" />
+        </Link>
       </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1 gap-2">
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link
+                className="btn min-w-56 bg-blue-700  border-gray-300 text-gray-200 hover:bg-blue-600 hover:text-white hover:underline hover:border-white hover:decoration-white "
+                to={link.path}
+              >
+                {link.text}
+              </Link>
+            </li>
+          ))}
+          <div className="dropdown dropdown-end  flex">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar border border-white"
+            >
+              {nombreUs}
+              {apellidoUs}
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-blue-700 rounded-box z-[1] mt-3 w-52 p-2 shadow text-white"
+            >
+              <li>
+                <a className="justify-between">Profile</a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </ul>
+      </div>
+    </div>
   );
-};
+}
 
 export default Navbar;
 
